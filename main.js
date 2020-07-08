a0f0a1e303a5ab92d0323e56a087f07d00f20650
heojh <jihaeng1989@gmail.com>
2020-07-08 12:08:13 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
@@ -14,7 +13,6 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
@@ -27,7 +25,6 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
-
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
@@ -37,6 +34,21 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
 });
 
 
