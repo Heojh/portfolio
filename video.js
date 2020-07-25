35e506c08eca97bf92cc7014aa8642e7de76bc70
heojh <jihaeng1989@gmail.com>
2020-07-25 12:31:35 +0900

    JS: Modal video control

---
+++ video.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(function() {
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "";
+        $(theModal + ' source').attr('src', videoSRCauto);
+        $(theModal + ' video').load();
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' source').attr('src', videoSRC)
+            $(theModal + ' video').trigger('pause');
+        });
+    });
+});
